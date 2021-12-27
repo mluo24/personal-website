9372f03774b70f6d7dcdc1e61ba3516dfa81a364
Miranda Luo <miranda.luo24@gmail.com>
2021-12-27 00:16:40 -0500

    added support for MUI

---
+++ plugins/gatsby-plugin-top-layout/TopLayout.tsx
@@ -0,0 +1,28 @@
+import * as React from "react"
+import { Helmet } from "react-helmet"
+import CssBaseline from "@mui/material/CssBaseline"
+import { ThemeProvider } from "@mui/material/styles"
+import theme from "../../src/theme"
+
+interface propTypes {
+  children: React.Component
+}
+
+export default function TopLayout(props: propTypes) {
+  return (
+    <React.Fragment>
+      <Helmet>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        {props.children}
+      </ThemeProvider>
+    </React.Fragment>
+  )
+}
