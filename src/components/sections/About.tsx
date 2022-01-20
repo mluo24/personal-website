e0fcdcfc54afb3a1868e20a95453f8f39ea08efb
mluo24 <miranda.luo24@gmail.com>
2022-01-20 17:20:17 -0500

    fixed border radius issue on Safari for image (again)

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -74,6 +74,9 @@ const ImageWrapper = styled(Box)(({ theme }) => ({
       paddingBottom: "100%",
     },
   },
+  ".selfimg div": {
+    borderRadius: "100%",
+  },
   ".selfimg img": {
     borderRadius: "100%",
   },
